3906532607baeab79f00294a949028462b83d202
Norman Littlejohn <nlittlejohn@kcftech.com>
2020-03-19 15:42:51 -0400

    Delete corp /w button

--- kcftech-react/src/Corporations.tsx
+++ kcftech-react/src/Corporations.tsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+type Corporation = {
+    id: number;
+    name: string;
+    icon: string;
+};
+
+const defaultCorporations = [
+    { id: 1, name: "Apple", icon: "apple.png" },
+    { id: 2, name: "Amazon", icon: "amzn.png" },
+    { id: 3, name: "Google", icon: "google.png" }
+];
 
 function Corporations() {
-    const corporations = [
-        { id: 1, name: "Apple", icon: "apple.png" },
-        { id: 2, name: "Amazon", icon: "amzn.png" },
-        { id: 3, name: "Google", icon: "google.png" }
-    ];
-
-    type Corporation = {
-        id: number;
-        name: string;
-        icon: string;
-    };
+    const [corporations, setCorporations] = useState(defaultCorporations);
 
     function renderCorp(corp: Corporation) {
         return (
             <tr>
+                <td>
+                    <button onClick={() => onDeleteClick(corp.id)}>
+                        Delete
+                    </button>
+                </td>
                 <td>{corp.id}</td>
                 <td>{corp.name}</td>
                 <td>{corp.icon}</td>
             </tr>
         );
+    }
+
+    function onDeleteClick(id: number) {
+        const newCorps = corporations.filter(corp => corp.id !== id);
+        setCorporations(newCorps);
     }
 
     return (
@@ -29,6 +41,7 @@ function Corporations() {
             <table>
                 <thead>
                     <tr>
+                        <th></th>
                         <th>ID</th>
                         <th>Name</th>
                         <th>Icon</th>
