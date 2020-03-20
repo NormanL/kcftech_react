c192b355318dd0b1941b6081b87018df34c76aae
Norman Littlejohn <nlittlejohn@kcftech.com>
2020-03-20 11:01:05 -0400

    Added cypress enhancements and test, updated scripts

---
+++ kcftech-react/cypress/integration/corporations.spec.js
@@ -0,0 +1,9 @@
+describe("Corporations", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+
+    it("should display a list of corporations", () => {
+        cy.findByText("Corporations");
+    });
+});
